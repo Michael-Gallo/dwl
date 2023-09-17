b5ef184c1fb4678b03889db9acd1b72995143e8d
Michael A Gallo <michaelgallo@protonmail.com>
2023-09-17 15:51:45 -0400

    smart borders

--- config.h
+++ config.h
@@ -1,6 +1,7 @@
 /* appearance */
 static const int sloppyfocus               = 0;  /* focus follows mouse */
 static const int bypass_surface_visibility = 0;  /* 1 means idle inhibitors will disable idle tracking even if it's surface isn't visible  */
+static const int smartborders              = 1;
 static const unsigned int borderpx         = 1;  /* border pixel of windows */
 static const float bordercolor[]           = {0.5, 0.5, 0.5, 1.0};
 static const float focuscolor[]            = {0.08627450980392157, 0.6235294117647059, 0.43529411764705883, 1.0};
