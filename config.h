a90210ce6ebc2a6c828332de72929682701875f0
Michael A Gallo <michaelgallo@protonmail.com>
2023-10-15 14:11:49 -0400

    inc/decmaster no longer have keybinds

--- config.h
+++ config.h
@@ -110,8 +110,8 @@ static const Key keys[] = {
 	/* modifier                  key                 function        argument */
 	{ MODKEY,                    XKB_KEY_j,          focusstack,     {.i = +1} },
 	{ MODKEY,                    XKB_KEY_k,          focusstack,     {.i = -1} },
-	{ MODKEY,                    XKB_KEY_i,          incnmaster,     {.i = +1} },
-	{ MODKEY,                    XKB_KEY_d,          incnmaster,     {.i = -1} },
+	// { MODKEY,                    XKB_KEY_i,          incnmaster,     {.i = +1} },
+	// { MODKEY,                    XKB_KEY_d,          incnmaster,     {.i = -1} },
 	{ MODKEY,                    XKB_KEY_h,          setmfact,       {.f = -0.05} },
 	{ MODKEY,                    XKB_KEY_l,          setmfact,       {.f = +0.05} },
 	{ MODKEY,                    XKB_KEY_semicolon,     zoom,           {0} },
